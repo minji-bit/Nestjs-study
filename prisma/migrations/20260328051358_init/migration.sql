bc7b53f590101e65c1d4e0bc9f691a854d25f08a
jeon min ji <jeon320@naver.com>
2026-03-28 14:22:27 +0900

    feat: NestJS+Prisma 실습(DB 모델링)

---
+++ prisma/migrations/20260328051358_init/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - The primary key for the `user` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `user` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE `user` DROP PRIMARY KEY,
+    ADD COLUMN `hashedRt` VARCHAR(191) NULL,
+    ADD COLUMN `password` VARCHAR(191) NOT NULL,
+    MODIFY `id` VARCHAR(191) NOT NULL,
+    MODIFY `name` VARCHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- CreateTable
+CREATE TABLE `Board` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `body` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Board` ADD CONSTRAINT `Board_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
